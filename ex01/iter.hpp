3c8f8b136a45957264fbd6a6ee38058ce6f7cff9
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-23 13:30:51 +0300

    [move] ex01 print(): -> iter.hpp

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -13,3 +13,13 @@ void iter(T array[], size_t length, void ( func )( T const& ))
 	for ( size_t i = 0; i < length; i++ )
 		func( array[ i ] );
 }
+
+// template< typename T >
+// void	print( T & x )
+// { std::cout << x << std::endl;
+// }
+
+template< typename T >
+void	print( const T & x )
+{ std::cout << "const" << x << std::endl;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,15 +5,13 @@ template< typename T >
 void	squareIt( T & x )
 { x *= x;
 }
-template< typename T >
-void	print( T & x )
-{ std::cout << x << std::endl;
-}
+
 
 int 	main() {
 	int	nums[] = { 11, 22, 33 };
 
 	iter( nums, 3, squareIt );
 	iter( nums, 3, print );
+
 	return 0;
 }
