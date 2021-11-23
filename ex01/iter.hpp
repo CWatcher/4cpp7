0339ed791dbf44db0439276f27f57ea003496a23
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-23 05:29:18 +0300

    [undone] + ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename  T >
+void iter(T array[], size_t length, void ( func )( T& ) )
+{
+	for ( size_t i = 0; i < length; i++ )
+		func( array[ i ] );
+}
+
+template <typename T>
+void iter(T array[], size_t length, void ( func )( T const& ))
+{
+	for ( size_t i = 0; i < length; i++ )
+		func( array[ i ] );
+}
