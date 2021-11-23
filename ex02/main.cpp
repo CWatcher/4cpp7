f6e22d3f8165aace4c192cd6e23a1a6d3f8d78d1
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-23 11:24:36 +0300

    [boilerplate] n = argv[ 1 ]

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,32 @@
 #include "Array.hpp"
 #include <iostream>
+#include <sstream>
 
-int	main()
+template < typename T >
+void test( Array< T > a )
 {
-	Array< int >	a0;
-	Array< int >	a20(20);
-
-	for ( unsigned i = 0; i < a20._n; i++ )
-		std::cout << a20._array[ i ] << " ";
+	for ( unsigned i = 0; i < a._n; i++ )
+		std::cout << a._array[ i ] << " ";
 	std::cout << std::endl;
+}
+
+int	main( int argc, char* argv[] )
+{
+
+	if ( argc == 1 )
+	{	Array< int >	a0;
+		test( a0 );
+		return 0;
+	}
+	std::stringstream	ss( argv[ 1 ]);
+	unsigned			n = 0;
+	ss >> n;
+	try
+	{
+		Array< int >	a( n );
+		test( a );
+	}
+	catch( const std::exception& e )
+	{	std::cout << "This is the end: " << e.what() << std::endl;
+	}
 }
--- ex02/t.sh
+++ ex02/t.sh
@@ -4,6 +4,9 @@ make > /dev/null		\
 	&& echo make ok! 	\
 	|| exit 1
 
-echo
-./aa
+set -v
 
+./aa -1
+./aa
+./aa 0
+./aa 10
